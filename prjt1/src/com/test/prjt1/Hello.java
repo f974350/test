1ef4b572763b48ace13fc08182455ecf06912e0d
f974350 <f974350@rmqkr.net>
2013-05-23 15:03:15 +0530

    Added Exclamations

--- prjt1/src/com/test/prjt1/Hello.java
+++ prjt1/src/com/test/prjt1/Hello.java
@@ -1,10 +1,9 @@
-
 package com.test.prjt1;
 
 public class Hello
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world !!");
+        System.out.println("Hello world !!!!!!");
     }
 }
